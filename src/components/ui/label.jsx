79e6454df99b20c45dd3e5bf5d5159d5c954d333
bistrohan16-rgb <bistrohan16@gmail.com>
2026-08-12 17:45:18 +0100

    Delete src directory

--- src/components/ui/label.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
-import { cn } from "@/utils"
-const Label = React.forwardRef(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root ref={ref} className={cn("text-xs font-semibold tracking-widest uppercase text-[#B8960C] font-body", className)} {...props} />
-))
-Label.displayName = "Label"
-export { Label }
